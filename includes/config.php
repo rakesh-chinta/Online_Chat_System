ac07031924985867cfe3ce3ca4a0d868df423611
Rakesh Naga Chinta <ch.rakesh003@gmail.com>
2017-02-25 21:57:56 +0530

    Add files via upload

---
+++ includes/config.php
@@ -0,0 +1,10 @@
+
+<?php
+$con = mysql_connect('localhost', '', '');
+	mysql_select_db("chatroom", $con);
+	if(!$con)
+	{
+		die('Could not connect: ' . mysql_error());
+	}
+
+?>
---
+++ includes/const.php
@@ -0,0 +1,5 @@
+
+<?php
+define(DOC_DIR,dirname(_FILE_));
+$db_file = DOC_DIR."/config.php";
+?>
